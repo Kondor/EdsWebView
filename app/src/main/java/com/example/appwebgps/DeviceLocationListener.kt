c18d41811795ecffd15ad2e81f1bff06bb1cd069
Kondor <f51.5@mail.ru>
2023-05-22 12:54:12 +0400

    delete old file

--- app/src/main/java/com/example/appwebgps/DeviceLocationListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appwebgps
-
-import android.location.Address
-
-interface DeviceLocationListener {
-    // функция callback onDeviceLocationChanged
-    // получение текущего местоположения при измнении местоположения устройства
-    fun onDeviceLocationChanged(results: List<Address>?)
-}
